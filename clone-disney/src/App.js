c5e048a31a9c64c0f3859722ba15f3ba699b0d94
DevvSakib <devvsakib@gmail.com>
2022-08-16 11:26:07 +0600

    Header Added

---
+++ clone-disney/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
