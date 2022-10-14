a6a2b354d1b99fb525bfd8eef6b9deda86d7e4dc
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-10-15 00:28:50 +0700

    feat: Home styles

--- PROJECTS/RealTimeChat/frontend/src/App.tsx
+++ PROJECTS/RealTimeChat/frontend/src/App.tsx
@@ -1,7 +1,16 @@
 import { FC } from 'react'
 
+import { Button, Container, InnerContainer, Input } from './AppStyles'
+
 const App: FC = () => {
-	return <div></div>
+	return (
+		<Container>
+			<InnerContainer>
+				<Input placeholder='Username' />
+				<Button>Join</Button>
+			</InnerContainer>
+		</Container>
+	)
 }
 
 export default App
---
+++ PROJECTS/RealTimeChat/frontend/src/AppStyles.tsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+	min-height: 100vh;
+	min-width: 100vw;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
+
+export const InnerContainer = styled.div`
+	display: flex;
+	justify-content: space-evenly;
+	align-items: center;
+
+	border: 3px solid black;
+
+	width: clamp(30%, 30%, 80%);
+	height: 150px;
+
+	border-radius: 15px;
+`
+
+export const Input = styled.input`
+	width: 70%;
+	height: 50px;
+
+	padding: 10px 20px;
+
+	border: 3px solid black;
+	border-radius: 15px;
+`
+
+export const Button = styled.button`
+	border-radius: 15px;
+	border: none;
+
+	width: 20%;
+	height: 50px;
+
+	background-color: #00aeff;
+
+	transition: background-color 0.5s, color 0.5s;
+
+	&:hover {
+		background-color: #0000ff;
+		color: white;
+	}
+`
