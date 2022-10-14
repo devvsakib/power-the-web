9bd8d7171b32367584e117657335ba957bb13a3c
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-10-15 01:50:14 +0700

    feat: Disconnect event

--- PROJECTS/RealTimeChat/backend/src/Listeners/index.js
+++ PROJECTS/RealTimeChat/backend/src/Listeners/index.js
@@ -5,11 +5,17 @@ import RoomMessageSendListener from './RoomMessageSend'
 const MainListener = socket => {
 	const { username } = socket.handshake.query
 
+	socket.broadcast.emit('users:new', { id: socket.id, username })
+
 	AddUser(socket.id, username)
 
 	socket.on('message:send', (...args) =>
 		RoomMessageSendListener(socket, ...args)
 	)
+
+	socket.on('disconnect', () => {
+		socket.broadcast.emit('users:disconnect', { id: socket.id })
+	})
 }
 
 export default MainListener
