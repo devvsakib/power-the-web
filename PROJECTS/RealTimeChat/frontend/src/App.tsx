3bac5214499d704ca594c1bd45563197df42ec9b
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-10-15 14:55:32 +0700

    feat: Connect to server

--- PROJECTS/RealTimeChat/frontend/src/App.tsx
+++ PROJECTS/RealTimeChat/frontend/src/App.tsx
@@ -1,13 +1,34 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+
+// eslint-disable-next-line import/no-named-as-default
+import io from 'socket.io-client'
 
 import { Button, Container, InnerContainer, Input } from './AppStyles'
 
 const App: FC = () => {
+	const [Username, SetUsername] = useState('')
+
+	const Connect = () => {
+		if (Username.length < 3) return alert('Username minimum length is 3')
+
+		const connection = io('ws://localhost:8080', {
+			query: {
+				username: Username,
+			},
+		})
+
+		console.log({ connection })
+	}
+
 	return (
 		<Container>
 			<InnerContainer>
-				<Input placeholder='Username' />
-				<Button>Join</Button>
+				<Input
+					placeholder='Username'
+					onChange={event => SetUsername(event.target.value)}
+					value={Username}
+				/>
+				<Button onClick={Connect}>Join</Button>
 			</InnerContainer>
 		</Container>
 	)
