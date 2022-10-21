e3df631d3095ced53cbe96bb3c36ca7ed6e0956a
DevvSakib <devvsakib@gmail.com>
2022-10-22 00:49:49 +0600

    converting website into react (#72)
    * Added the project.html+project.css (#46)
    
    very nice pr. dont forget to star the repo🤗
    
    * Update README.md
    
    * created React TODO List (#50)
    
    very nice pr
    
    * project that suggests homeopathic medicines (#51)
    
    * Nebula - A cryptocurrency bulletin (#52)
    
    very nice PR
    
    * Added the footer (#53)
    
    * Updated style.css
    
    Added the style for the footer class.
    
    * Updated project.html
    
    Added the footer
    
    * Updated index.html
    
    Added footer
    
    * Updated contributer.html
    
    Added the footer
    
    * Updated blog.html
    
    Added the footer.
    
    * disney clone projects issues: #55 (#56)
    
    * Added a react project (#57)
    
    * added contributors.json file (#59)
    
    very nice pr
    
    * Added Uber cloned app (#63)
    
    * Fixed a bug where contributor and project tab were not working (#64)
    
    * Update README.md
    
    * feat: created initial setup for contributors page (#65)
    
    Co-authored-by: DevvSakib <devvsakib@gmail.com>
    
    * Hamburger Navbar and UI changes (#67)
    
    very nice pr
    
    * Added a New Project called Data Manager (#68)
    
    very nice pr, you are welcome to our server https://discord.gg/xwRbDwnbMr
    
    * Added Details in the contributors list. (#69)
    
    * Added a New Project called Data Manager
    
    * Added Details in the contributors list.
    
    * Update contributor.html
    
    * Update contributor.html
    
    * added contributors page (#71)
    
    there was so many conflicts, and while reviewing, my mind get stuck😄now all fine
    
    * Update contributor.html
    
    * Reedited the file. (#70)
    
    * Added a New Project called Data Manager
    
    * Added Details in the contributors list.
    
    * Reedited the file.
    
    Co-authored-by: DevvSakib <devvsakib@gmail.com>
    
    Co-authored-by: Rahul Gupta <93125486+rahul8858@users.noreply.github.com>
    Co-authored-by: sandilya27 <113860300+sandilya27@users.noreply.github.com>
    Co-authored-by: Rijul Sarji <64856177+rijulsarji@users.noreply.github.com>
    Co-authored-by: Aishaa-26 <95750380+Aishaa-26@users.noreply.github.com>
    Co-authored-by: Pratyush Kumar <72880631+pratyushkr11@users.noreply.github.com>
    Co-authored-by: Pragya Sapkota <pragyasapkota2056@gmail.com>
    Co-authored-by: Ayomide Adedeji <68964863+DiamonndAde@users.noreply.github.com>
    Co-authored-by: SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
    Co-authored-by: Nwani Victory <vickywane@gmail.com>
    Co-authored-by: Bhoopesh <82646284+bhoopesh369@users.noreply.github.com>
    Co-authored-by: Akshat Tiwari <akshattiwari0901@gmail.com>
    Co-authored-by: uday kahar <81680319+udaykahar@users.noreply.github.com>

---
+++ PROJECTS/React Gym Exercise App/src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+};
+
+export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+        'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+};
+
+export const fetchData = async (url, options) => {
+    const response = await fetch(url, options);
+    const data = response.json();
+
+    return data;
+}
---
+++ PROJECTS/React TODO List/todo/src/component/Todo.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+
+const Todo=({text,todo,todos,setTodos})=>{
+    const deleteHandler=()=>{
+        setTodos(todos.filter((el) => el.id !== todo.id));
+    };
+    const completeHandler=()=> {
+        setTodos(todos.map((item)=>{
+            if(item.id===todo.id){
+                return{
+                    ...item,completed: !item.completed
+                };
+            }
+            return item;
+        }))
+    }
+    return(
+        <div className="todo">
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
+            
+            <button onClick={completeHandler} className='complete-btn'>
+            <i className="fas fa-check">
+            </i>
+            </button>
+            <button onClick={deleteHandler} className='trash-btn'>
+                <i className="fas fa-trash"></i>
+            </button>
+        </div>
+    );
+}
+
+
+export default Todo;
