0de9c9f31161178b61f916f9d6d6d11120a1376a
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-10-22 14:35:59 +0530

    Projects Folder Deleted

--- PROJECTS/RealTimeChat/backend/src/Utils/Logger/Format/RemoveInfo.js
+++
@@ -1,7 +0,0 @@
-import winston from 'winston'
-
-const RemoveInfo = winston.format(info => {
-	return info.level !== 'info' ? info : false
-})
-
-export default RemoveInfo
--- PROJECTS/netflix-clone-using-react-main/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from './home';
-export { default as Browse } from './browse';
-export { default as Signin } from './signin';
-export { default as Signup } from './signup';
