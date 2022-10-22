ca0b4cf189d88a283668988c7536ed878afeac1e
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-10-22 11:28:54 +0530

    Footer Done

--- main-website/src/App.js
+++ main-website/src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./component/Footer/Footer";
 import Navbar from './component/Navbar/Navbar';
 import "./main.css";
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' exact ></Route>
         </Routes>
+        <Footer/>
       </Router>
     </div>
   );
