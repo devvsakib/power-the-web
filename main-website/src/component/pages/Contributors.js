7ab255cb44b3fa85b47b2cfec2e58beba242e2f4
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-10-24 01:28:28 +0530

    Profile Card Done & Added My Name to Contributors

--- main-website/src/component/pages/Contributors.js
+++ main-website/src/component/pages/Contributors.js
@@ -3,17 +3,17 @@ import data from "../../Contributors.json";
 
 function Contributors() {
   return (
-    <div className="container">
-      <div className="details text-center text-uppercase">
-        <h1 className="title text-3xl"> Our Contributors </h1>
-        <p className="sub-title"> Thanks To All Contributors. Join Us </p>
+    <div className="w-full">
+      <div className="text-center text-uppercase m-8">
+        <h1 className="text-4xl font-semibold"> Our Contributors </h1>
+        <p className="text-base"> Thanks To All Contributors. Join Us </p>
       </div>
 
       <div>
-        {data.profiles.map(({ name, github, twitter, avatarUrl }) => {
-          return (
-            <ul className="contributors-list grid gap-4 place-items-center" id="contributors" >
-              <div className="contributor-card m-1 w-[15rem]">
+        <ul className="grid gap-x-4 gap-y-4 place-items-center grid-cols-custom">
+          {data.profiles.map(({ name, github, twitter, avatarUrl }) => {
+            return (
+              <div className="contributor-card m-1 w-60" key={github}>
                 <div className="rounded-3xl overflow-visible shadow-xl max-w-[240px] h-[350px] my-3 p-4">
                   <img
                     src={avatarUrl}
@@ -21,29 +21,31 @@ function Contributors() {
                     alt="avatarUrl"
                   />
                   <div>
-                    <h2 className="text-center text-2xl font-bold">${name}</h2>
+                    <h2 className="text-center text-2xl font-bold m-2">
+                      {name}
+                    </h2>
                     <div className="flex justify-evenly items-center ">
-                      <a href="https://github.com/${github}">
+                      <a href={`https://github.com/${github}`}>
                         <img
                           src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
                           className="w-10"
                           alt="github"
                         />
                       </a>
-                      <a href="https://twitter.com/${twitter}">
+                      <a href={`https://twitter.com/${twitter}`}>
                         <img
                           src="https://cdn-icons-png.flaticon.com/512/2111/2111738.png"
                           className="w-8"
-                          alt="tritter"
+                          alt="twitter"
                         />
                       </a>
                     </div>
                   </div>
                 </div>
               </div>
-            </ul>
-          );
-        })}
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
