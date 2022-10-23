07242f77e63cc827d8e18ed3c960c5b0b3aef607
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-10-23 23:21:08 +0530

    Moved  Contributors.json to src repo & Fetched JSON Data

--- main-website/src/component/pages/Contributors.js
+++ main-website/src/component/pages/Contributors.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import data from "../../Contributors.json";
 
 function Contributors() {
   return (
     <div>
       <h1>Contributors</h1>
+      {data.profiles.map((profile) => {
+        return <h1>{profile.name}</h1>;
+      })}
     </div>
-  )
+  );
 }
 
-export default Contributors
+export default Contributors;
