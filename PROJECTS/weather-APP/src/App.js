c25fb02e05e5bb3b47edaf458e54bf9cc09dde59
DevvSakib <devvsakib@gmail.com>
2022-10-27 22:54:41 +0600

    solved git add issue

--- PROJECTS/weather-APP/src/App.js
+++ PROJECTS/weather-APP/src/App.js
@@ -1,28 +1,10 @@
-import { useEffect } from "react";
-
-import Card from "./components/Card";
-import Input from "./components/Input";
-import Button from "./components/Button";
-import { useWeather} from './context/Weather'
-import "./App.css";
+import Temp from "./component/wether/temp"
 
 function App() {
-  const weather = useWeather();
-
-  useEffect(() => {
-   // get current location here
-   weather.fetchCurrentUserLocationData();
-  },[]);
   return (
-    <div>
-      <h1 className = "heading">Weather Forecast</h1>
-      
-      <Input/>
-      <Button onClick={weather.fetchData} value = "Search"/>
-      <Card/>
-      <Button  value = "Refresh"/>
-      
-    </div>
+   <>
+   <Temp />
+   </>
   );
 }
 
--- PROJECTS/weather-APP/src/index.js
+++ PROJECTS/weather-APP/src/index.js
@@ -4,18 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import {WeatherProvider} from "./context/Weather";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <WeatherProvider>
-      <App/>
-    </WeatherProvider>
-    
+    <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
