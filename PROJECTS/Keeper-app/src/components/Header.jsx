2a9e492a06f628f4a77fb5360af0869dc46ea7ce
Anoosh MR <anooshmangalath@gmail.com>
2022-10-30 11:06:28 +0530

    Keeper app similar to google keep

---
+++ PROJECTS/Keeper-app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DescriptionIcon from '@mui/icons-material/Description';
+
+function Header() {
+  return (
+    <header>
+      <h1><DescriptionIcon style={{fontSize:35}}/> Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
