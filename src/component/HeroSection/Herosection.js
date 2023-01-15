e6473c95be650de637dfcbccf5ddb8da1b0e811e
DevvSakib <devvsakib@gmail.com>
2023-01-15 23:49:13 +0600

    homepage design improved

--- src/component/HeroSection/Herosection.js
+++ src/component/HeroSection/Herosection.js
@@ -4,17 +4,9 @@ function Herosection() {
   return (
     <>
       <div className='hero-section flex justify-between py-10 items-center '>
-        <div className='hero-content pb-10'>
-          <h2 className=' text-5xl font-bold text'>PowerTheWeb</h2>
+        <div className='hero-content pb-10 mx-auto text-center'>
+          <h2 className='text' id="tet">PowerTheWeb</h2>
           <p className='mt-5 text-xl'>An opensouce Projects for Web Developer.<br/> Start your development from here.</p>
-        </div>
-        <div className='dc-frame'> 
-          <iframe
-            src="https://discord.com/widget?id=1014618195174363157&theme=dark"
-            allowtransparency="true"
-            frameborder="0"
-            sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
-          ></iframe>
         </div>
       </div>
     </>
