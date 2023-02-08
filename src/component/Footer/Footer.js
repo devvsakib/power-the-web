3604644613939f39e40dd6b35d53119426038701
Jason <jasondsouza212@gmail.com>
2023-02-08 11:55:50 +0530

    Changed the hero title color to logo color

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="w-full bg-black">
       <p className="text-white p-2 flex justify-center text-center tracking-widest">
-        PowerTheWeb | Developed By PTW Community @ 2022
+        PowerTheWeb | Developed By PTW Community @ 2023
       </p>
     </footer>
   );
