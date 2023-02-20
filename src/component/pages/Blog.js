c43d85aa552f7539e473c8091e6eb803d4867223
DevvSakib <devvsakib@gmail.com>
2023-02-20 12:26:42 +0600

    blog card height fixed, blog added

--- src/component/pages/Blog.js
+++ src/component/pages/Blog.js
@@ -1,25 +1,29 @@
 import { React } from "react";
-import BlogCard from "../../json/BlogCards.json";
+import BlogCard from "../../json/Blogs.json";
 
 function Blog() {
   return (
     <div className="flex flex-wrap justify-center my-6">
-      {BlogCard.Cards.map((card) => (
+      {BlogCard.Cards.map((card, idx) => (
         <div
-          key={card.key}
-          className="w-100 h-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 m-3 bg-slate-50 rounded"
+          key={idx}
+          className="w-100 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 m-3 bg-slate-50 rounded"
         >
-          <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img className="w-full" src={card.img} alt={card.title} />
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">{card.title}</div>
+          <div className="max-w-sm  md:h-[450px] flex flex-col justify-between rounded overflow-hidden shadow-lg">
+            <div>
+              <img className="w-full h-44 object-cover" src={card.thumbnail} alt={card.title} />
+              <h2 className="px-6 font-bold text-lg my-2">{card.title}</h2>
+            </div>
+            <div className="px-6 pb-4">
               <p className="text-gray-700 text-base">
                 {card.description.slice(0, 100) + "..."}
               </p>
             </div>
             <div className="px-6 pb-6">
               <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Read More
+                <a href={card.url} target="_blank">
+                  Read More
+                </a>
               </button>
             </div>
           </div>
