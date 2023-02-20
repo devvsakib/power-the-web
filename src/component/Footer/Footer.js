b67638e46b891785d8b50b0579f4f377a58187c5
DevvSakib <devvsakib@gmail.com>
2023-02-20 12:48:46 +0600

    footer year is now dynamic

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="w-full bg-black">
       <p className="text-white p-2 flex justify-center text-center tracking-widest">
-        PowerTheWeb | Developed By PTW Community @ 2023
+        PowerTheWeb | Developed By PTW Community @ {new Date().getFullYear()}
       </p>
     </footer>
   );
