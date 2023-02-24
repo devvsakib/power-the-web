de780d5c071f76e06f6acd070ae00bc4135de8ac
Shaik Dadapeer <69897636+DaduDev@users.noreply.github.com>
2023-02-24 16:22:21 +0530

    Update in projects (#176)
    * Update Blogs.json
    
    * Update Contributors.json
    
    * Add files via upload

---
+++ PROJECTS/amazon-clone/src/Product.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Product.css";
+import { useStateValue } from "./StateProvider";
+
+function Product({ id, title, image, price, rating }) {
+  const [{}, dispatch] = useStateValue();
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_) => (
+              <h4>⭐️</h4>
+            ))}
+        </div>
+      </div>
+      <img src={image} alt="" />
+      <button onClick={addToBasket}>Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
