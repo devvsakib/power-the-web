3fcbee1baa1d28e68b0f3df2b8105205e1740d8e
DevvSakib <devvsakib@gmail.com>
2023-02-28 15:44:24 +0600

    card shadow added

--- src/component/Guidelines/pages/Cards.js
+++ src/component/Guidelines/pages/Cards.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 const Cards = ({title, summarize}) => {
     return (
-        <div className="border-l-2 m-3 bg-white/10 backdrop-blur-[10px] text-white rounded-lg p-3">
+        <div className="border-l-2 m-3 bg-white/10 backdrop-blur-[10px] text-white rounded-lg p-3 hover:shadow-lg hover:shadow-[#8811ee] cursor-pointer">
             <div className="max-w-sm flex flex-col justify-between rounded overflow-hidden">
                 <div>
                     <h2 className="px-6 font-bold text-lg my-2">{title}</h2>
@@ -14,7 +14,7 @@ const Cards = ({title, summarize}) => {
                 <div className="px-6 pb-6">
                     <button className="ptw-grad hover:scale-110 duration-300 ease-linear hover:shadow-md hover:shadow-white/30 ptw-gradHover text-white font-bold py-2 px-4 rounded">
                         <Link>
-                            Read More
+                            Explore
                         </Link>
                     </button>
                 </div>
