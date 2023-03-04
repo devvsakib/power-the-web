0d820dbc2aa1871970125825ad8f1b5a96cb03f1
DevvSakib <devvsakib@gmail.com>
2023-03-04 11:43:04 +0600

    document page margin removed, docs added

---
+++ src/component/Guidelines/List/FrontendInterview.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useState } from 'react';
+
+const FrontendInterview = () => {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <div>
+            <h2 className='cursor-pointer'
+            onClick={()=> setOpen(!open)}
+            >Frontend Interview</h2>
+            <div className='flex flex-col gap-5'>
+                <ul className={`${open ? "" : "hidden"}`}>
+                    <li className='cursor-pointer'>HTML</li>
+                    <li className='cursor-pointer'>CSS</li>
+                    <li className='cursor-pointer'>JavaScript</li>
+                    <li className='cursor-pointer'>React</li>
+                    <li className='cursor-pointer'>Redux</li>
+                    <li className='cursor-pointer'>React Router</li>
+                    <li className='cursor-pointer'>React Hooks</li>
+                    <li className='cursor-pointer'>React Native</li>
+                    <li className='cursor-pointer'>Next.js</li>
+                    <li className='cursor-pointer'>TypeScript</li>
+                    <li className='cursor-pointer'>Styled Components</li>
+                    <li className='cursor-pointer'>Tailwind CSS</li>
+                    <li className='cursor-pointer'>Bootstrap</li>
+                    <li className='cursor-pointer'>Sass</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default FrontendInterview
--- src/component/Guidelines/pages/Documents.js
+++ src/component/Guidelines/pages/Documents.js
@@ -1,29 +1,20 @@
-import { useParams } from 'react-router-dom';
+import BackendRoadmap from '../List/BackendRoadmap';
+import FrontendInterview from '../List/FrontendInterview';
+import FrontendRoadmap from '../List/FrontendRoadmap';
+import FullStackRoadmap from '../List/FullStackRoadmap';
+
 const Documents = () => {
-    // const { page } = useParams();
-    // console.log(page);
     return (
         <div className='h-[86vh] overflow-y-hidden text-fuchsia-200 bg-white/10 backdrop-blur-[10px]'>
-            <div className='flex flex-col md:flex-row mt-[20rem]'>
+            <div className='flex flex-col md:flex-row'>
                 <div className='w-full  md:w-1/4  rounded-lg p-3'>
                     <h1 className='text-2xl font-bold'>List of Documents</h1>
-                    <ul className='h-[55%] overflow-y-scroll'>
-                        <li className='mb-2 p-3'>Document 1</li>
-                        <li className='mb-2 p-3'>Document 2</li>
-                        <li className='mb-2 p-3'>Document 3</li>
-                        <li className='mb-2 p-3'>Document 4</li>
-                        <li className='mb-2 p-3'>Document 5</li>
-                        <li className='mb-2 p-3'>Document 6</li>
-                        <li className='mb-2 p-3'>Document 7</li>
-                        <li className='mb-2 p-3'>Document 8</li>
-                        <li className='mb-2 p-3'>Document 9</li>
-                        <li className='mb-2 p-3'>Document 10</li>
-                        <li className='mb-2 p-3'>Document 11</li>
-                        <li className='mb-2 p-3'>Document 12</li>
-                        <li className='mb-2 p-3'>Document 13</li>
-                        <li className='mb-2 p-3'>Document 14</li>
-                        <li className='mb-2 p-3'>Document 15</li>
-                    </ul>
+                    <div className='h-auto pb-5 mt-5 flex flex-col gap-10 overflow-y-scroll'>
+                        <FrontendInterview />
+                        <FrontendRoadmap />
+                        <BackendRoadmap />
+                        <FullStackRoadmap />
+                    </div>
                 </div>
                 <div className='w-full md:w-3/4 px-10 bg-white/10 backdrop-blur-[10px] rounded-lg p-3'>
                     <h1 className='text-2xl font-bold'>Document 1</h1>
@@ -45,10 +36,3 @@ const Documents = () => {
 }
 
 export default Documents
-{/* Design concept
-    1. this page will be 2part. 1st part will be the list of all the documents. 2nd part will be the content of the document.
-    2. list will be in the left side of the screen and content will be in the right side of the screen.
-    3. left side is a sidebar.
-    4. like tailwindcss.com/docs
-    
-*/}
