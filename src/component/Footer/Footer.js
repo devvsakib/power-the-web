c13cc5aa7c666e1d9eb064893c983b3587381ee5
k15 <krisnantobiyuh@gmail.com>
2023-03-06 23:09:11 +0700

    fix : layout home page

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="w-full backdrop-blur-[5px] bg-white/10">
-      <p className="text-white p-2 flex justify-center text-center tracking-widest font-semibold">
+      <p className="text-white p-1 flex justify-center text-center tracking-widest font-semibold">
         PowerTheWeb | Developed By PTW Community @ {new Date().getFullYear()}
       </p>
     </footer>
