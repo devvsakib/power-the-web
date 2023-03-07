bcf5a9ae87d93abaabf4148a6656ead736bcb46b
k15 <krisnantobiyuh@gmail.com>
2023-03-07 09:01:44 +0700

    fix : Warning: Each child in a list should

--- src/component/Projects/GetProjects.js
+++ src/component/Projects/GetProjects.js
@@ -36,7 +36,7 @@ function GetProjects() {
                             <ul className='grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 items-center'>
                                 {
                                     projects.map((item, index) => (
-                                        <li className='py-3 items-center grid'>
+                                        <li className='py-3 items-center grid' key={item.name}>
                                             <a title={item.name} className='rounded-l-[30px] flex align-middle bg-gradient-to-r to-[#00000018] from-[#b700ff48] backdrop-filter backdrop-blur-md z-0 overflow-visible shadow-md text-white hover:from-[#3701729e] hover:to-[#7128b3b8] transition-all ease-linear delay-80 duration-300 hover:scale-110 hover:transition-all' href={item.html_url} target="_blank" rel="noopener noreferrer">
                                                 <span className='bubble h-14 w-14 cursor-pointer border-2 border-[#adb2c5] rounded-full text-center font-bold flex items-center justify-center'>{index + 1}</span>
                                                 <p className='pl-2 font-semibold flex items-center tracking-wider'>{item.name.substring(0, 28)} {item.name.length >= 28 && '...'}</p>
