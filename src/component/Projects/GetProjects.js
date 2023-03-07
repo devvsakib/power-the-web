6c210c3fd97cec5ce8c0fb28cf0fee8dd5c7d071
k15 <krisnantobiyuh@gmail.com>
2023-03-07 09:00:35 +0700

    style : update style project list page

--- src/component/Projects/GetProjects.js
+++ src/component/Projects/GetProjects.js
@@ -1,20 +1,28 @@
 import { React, useState, useEffect } from 'react'
 import axios from 'axios'
 import './Projects.css'
+import Error from '../Error/Error.js'
 function GetProjects() {
     const url = "https://api.github.com/repos/devvsakib/power-the-web/contents/PROJECTS"
     const [projects, setProjects] = useState([])
     const [isLoading, setIsLoading] = useState(true)
+    const [isError, setIsError] = useState(false)
     const getProjects = async () => {
         await axios.get(url)
-            .then(res =>
-                setProjects(res.data)
-            )
+            .then(res => setProjects(res.data))
+            .catch(() => setIsError(true))
             .finally(() => setIsLoading(false))
     }
     useEffect(() => {
-        getProjects()
+        getProjects();
     }, [projects.length])
+
+    if (isError) {
+        return <Error
+            msg={`Sorry, we couldn’t find project list for now. Please comeback letter after`}
+        />
+    }
+
     return (
         <>
             {
@@ -29,9 +37,9 @@ function GetProjects() {
                                 {
                                     projects.map((item, index) => (
                                         <li className='py-3 items-center grid'>
-                                            <a className='flex projects-line align-middle bg-black rounded-l-[30px]' href={item.html_url} target="_blank" rel="noopener noreferrer">
-                                                <span className='h-14 w-14 cursor-pointer border-2 border-black rounded-full text-center font-bold flex items-center justify-center'>{index + 1}</span>
-                                                <p className='pl-2  font-semibold flex items-center tracking-wider'>{item.name}</p>
+                                            <a title={item.name} className='rounded-l-[30px] flex align-middle bg-gradient-to-r to-[#00000018] from-[#b700ff48] backdrop-filter backdrop-blur-md z-0 overflow-visible shadow-md text-white hover:from-[#3701729e] hover:to-[#7128b3b8] transition-all ease-linear delay-80 duration-300 hover:scale-110 hover:transition-all' href={item.html_url} target="_blank" rel="noopener noreferrer">
+                                                <span className='bubble h-14 w-14 cursor-pointer border-2 border-[#adb2c5] rounded-full text-center font-bold flex items-center justify-center'>{index + 1}</span>
+                                                <p className='pl-2 font-semibold flex items-center tracking-wider'>{item.name.substring(0, 28)} {item.name.length >= 28 && '...'}</p>
                                             </a>
                                         </li>
                                     ))
