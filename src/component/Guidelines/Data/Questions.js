dc9f70b5da47714074866629a7ba8dd292e2f402
DevvSakib <devvsakib@gmail.com>
2023-03-14 21:39:12 +0600

    questions banner added

--- src/component/Guidelines/Data/Questions.js
+++ src/component/Guidelines/Data/Questions.js
@@ -28,15 +28,21 @@ const Questions = ({ content }) => {
 
     return (
         <div>
-            {
-                !questions.length ? <h1 className='text-2xl font-bold'>No Questions Found</h1> :
-                    questions.map((question, idx) => (
-                        <div key={idx} className='bg-[#242A31]/5 backdrop-blur-sm text-[#242A31] rounded p-5 my-5'>
-                            <h1 className='font-semibold text-2xl font-[raleway] mb-2'>{question.question}</h1>
-                            <p className=''>{question.answer}</p>
-                        </div>
-                    ))
-            }
+            <div>
+                {/* <h1 className='text-2xl font-bold'>{content.toUpperCase()} Questions</h1> */}
+                <img className='mx-auto w-full' src='/images/jsInterviewthumb.png' />
+            </div>
+            <div>
+                {
+                    !questions.length ? <h1 className='text-2xl font-bold'>No Questions Found</h1> :
+                        questions.map((question, idx) => (
+                            <div key={idx} className='bg-[#242A31]/5 backdrop-blur-sm text-[#242A31] rounded p-5 my-5'>
+                                <h1 className='font-semibold text-2xl font-[raleway] mb-2'>{question.question}</h1>
+                                <p className=''>{question.answer}</p>
+                            </div>
+                        ))
+                }
+            </div>
         </div>
     )
 }
