9a2b0afed16db3f1d13218be7148850f490ef5ba
k15 <krisnantobiyuh@gmail.com>
2023-03-20 09:21:34 +0700

    fix : overlap footer and font style

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="w-full backdrop-blur-[5px] bg-white/10 flex items-center justify-center px-5 py-5">
-      <p className="text-white flex justify-center text-center tracking-widest font-semibold">
+      <p className="text-sm text-white flex justify-center text-center tracking-widest font-semibold">
         PowerTheWeb | Developed By PTW Community @ {new Date().getFullYear()}
       </p>
       <a
