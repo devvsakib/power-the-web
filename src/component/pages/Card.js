96f1526b0667c528bfba4e521428ce9cb2459e76
k15 <krisnantobiyuh@gmail.com>
2023-04-04 10:40:35 +0700

    fix(contributor card): fixing warning eslint

--- src/component/pages/Card.js
+++ src/component/pages/Card.js
@@ -121,7 +121,7 @@ const Card = () => {
                   </div>
                 </Fragment>
             }
-          <p className='font-[500] mt-[15px] text-white text-md'>See the docs <a href="https://github.com/devvsakib/power-the-web/blob/main/docs/contributor-card.md" target="_blank" className='text-[#eac20d]'>Here</a> how to add your project to your contributor card</p>
+          <p className='font-[500] mt-[15px] text-white text-md'>See the docs <a href="https://github.com/devvsakib/power-the-web/blob/main/docs/contributor-card.md" target="_blank" className='text-[#eac20d]' rel="noreferrer">Here</a> how to add your project to your contributor card</p>
           </div>
         </div>
       </div>
