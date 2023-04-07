04b09d1d06b8d8c85fe2b91219d76a751889552a
DevvSakib <devvsakib@gmail.com>
2023-04-07 22:43:13 +0600

    removed card hover added

--- src/component/Contributors/CProfileCard.js
+++ src/component/Contributors/CProfileCard.js
@@ -5,7 +5,9 @@ import { Link } from 'react-router-dom';
 const CProfileCard = ({ github, avatarUrl, twitter, name, idx }) => {
     return (
         <div className="m-1 w-full md:w-30 cursor-pointer" key={idx}>
-            <div className="w-full mx-auto rounded-3xl bg-gradient-to-r to-[#0f1c8018] from-[#b700ff48] backdrop-filter backdrop-blur-md z-0 overflow-visible shadow-md max-w-[240px] h-[350px] my-3 p-4 text-white">
+            <div className="w-full mx-auto rounded-3xl bg-gradient-to-r
+                                to-[#0f1c8018] from-[#b700ff48] backdrop-filter backdrop-blur-md z-0 overflow-visible shadow-md max-w-[240px] h-[350px] my-3 p-4 text-white
+                                hover:from-[#9d08d8] hover:to-[#711fce] transition-all ease-linear delay-80 duration-300 hover:scale-105 hover:transition-all">
                 <img src={avatarUrl} className="rounded-full w-[100%] h-[65%] skeleton  border-solid border-white border-2 mt-3"
                     alt={name}
                 />
@@ -16,22 +18,22 @@ const CProfileCard = ({ github, avatarUrl, twitter, name, idx }) => {
                     <div className="flex justify-evenly items-center mt-4 ">
                         <a target="_blank" href={`https://github.com/${github}`} rel="noopener noreferrer">
                             <FaGithub
-                                className="w-8 h-8 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+                                className="socialIcon"
                                 alt="github"
                             />
                         </a>
                         {
                             github &&
-                                <Link to={`/cards/${github}`} title='My Contributor Card'>
-                                    <FaRegAddressCard
-                                        className="w-8 h-8 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110"
-                                        alt="twitter"
-                                    />
-                                </Link>
+                            <Link to={`/cards/${github}`} title='My Contributor Card'>
+                                <FaRegAddressCard
+                                    className="socialIcon"
+                                    alt="twitter"
+                                />
+                            </Link>
                         }
                         <a target="_blank" href={`https://twitter.com/${twitter}`} rel="noopener noreferrer">
                             <FaTwitter
-                                className="w-8 h-8 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+                                className="socialIcon"
                                 alt="twitter"
                             />
                         </a>
