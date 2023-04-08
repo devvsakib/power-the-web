32c97eab38e491e8ecbe66eee9c1a03d4e15e60f
Nelson Uprety <nelsonuprety3@gmail.com>
2023-04-08 12:39:23 +0100

    Optimized the useWindow custom hook

--- src/component/hooks/useWindow.js
+++ src/component/hooks/useWindow.js
@@ -1,29 +1,59 @@
 //  write a custom hook to get the window size
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 const useWindow = () => {
-    const [windowSize, setWindowSize] = useState({
-        width: undefined,
-        height: undefined,
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  /*
+   Added  the useCallback hook to memoize the handleResize function.
+   This could improve performance by avoiding unnecessary re-renders of the 
+   component which will use this hook.
+*/
+
+  /*
+  Used the requestAnimationFrame API to debounce the handleResize function.
+   This could improve performance by reducing the number of times the function 
+   is called during window resizing.
+*/
+
+  /**
+ Debouncing is a technique used to improve performance by
+  reducing the frequency of an action. 
+  In the context of window resizing, debouncing refers to delaying the execution of 
+  a function until a certain amount of time has passed since the last time 
+  the event was fired.
+ */
+
+  /*
+Using the requestAnimationFrame API to debounce the handleResize function can
+ improve performance by delaying its execution until the next animation frame. 
+ This means that the function will only be executed once per animation frame, 
+ regardless of how many times the resize event is fired.
+
+ */
+
+  const handleResize = useCallback(() => {
+    requestAnimationFrame(() => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
     });
+  }, []);
 
-    useEffect(() => {
-        function handleResize() {
-            setWindowSize({
-                width: window.innerWidth,
-                height: window.innerHeight,
-            });
-        }
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
 
-        window.addEventListener('resize', handleResize);
+    handleResize();
 
-        handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, [handleResize]);
 
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    return windowSize;
-}
+  return windowSize;
+};
 
 export default useWindow;
